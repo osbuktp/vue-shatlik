6b429cc85e69ca9ee6bb99182b695ef4d10bda57
buktp <mntnscr@gmail.com>
2017-08-25 17:23:24 +0300

    Initial

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app-wrapper">
+    <hamburger></hamburger>
+    <sidebar></sidebar>
+  </div>
+</template>
+
+<script>
+import Hamburger from './components/Sidebar/Hamburger.vue';
+import Sidebar from './components/Sidebar/Sidebar.vue';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Test'
+    }
+  },
+  components: {
+    Hamburger,
+    Sidebar
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="sidebar" :class="{sidebar_opened: opened}">
+    </div>
+</template>
+
+<script>
+import EventBus from '../../EventBus.vue';
+
+export default {
+  data() {
+      return {
+          opened: false
+      }
+  },
+  methods: {
+      toggleSidebar() {
+          this.opened = !this.opened;
+      }
+  },
+  mounted() {
+      EventBus.$on('toggleSidebar', this.toggleSidebar);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+    @media (min-width: 900px) {
+        display: none;
+    };
+    @media (orientation: portrait) {
+        width: 100vw;
+        transform: translate3d(-100vw, 0px, 0px);
+    };
+    @media (orientation: landscape) {
+        width: 100vh;
+        transform: translate3d(-100vh, 0px, 0px);
+    };
+    position: fixed;
+    z-index: 2;
+    height: 100%;
+    transition: transform .3s;
+    background-color: #03A9F4;
+    overflow-y: auto;
+}
+.sidebar_opened {
+    transform: translate3d(0px, 0px, 0px);
+}
+</style>
+
