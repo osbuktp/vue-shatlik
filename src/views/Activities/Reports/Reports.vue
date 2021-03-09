2afd208f05e92d510b5bd27a5202ec9c2bcd25bb
osbuktp <os.buktp@yandex.ru>
2021-03-09 19:43:35 +0300

    0903 fixes

--- src/views/Activities/Reports/Reports.vue
+++ src/views/Activities/Reports/Reports.vue
@@ -1,32 +1,52 @@
 <template>
-    <section class="content section">
-        <header>
-            <h1>Отчеты</h1>
-        </header>
-        <article class="media">
-            <div class="media-content">
-                <div class="content">
-                    <p>
-                        План финансово-хозяйственной деятельности АНО "ЦСП "Шатлык" ("Радость") на 2018 год
-                    </p>
-                </div>
-                <nav class="level">
-                    <a target="_blank" href="/documents/ПЛАН ФХД 1 - 2019 год.jpg" class="button is-link">План ФХД 1 (jpg)</a>
-                    <a target="_blank" href="https://docs.google.com/gview?url=https://shatlik.ru/documents/ПЛАН+ФХД+2+-+2019+год.docx" class="button is-link">План ФХД 2 (docx)</a>
-                    <a target="_blank" href="/documents/ПЛАН ФХД 3 - 2019 год.jpg" class="button is-link">План ФХД 3 (jpg)</a>
-                    <a target="_blank" href="https://docs.google.com/gview?url=https://shatlik.ru/documents/Отчет+о+деят-ти+НО.xlsm" class="button is-link">Отчет о деятельности НО</a>
-                    <a target="_blank" href="https://docs.google.com/gview?url=https://shatlik.ru/documents/Отчет+о+расход.дс.xlsm" class="button is-link">Отчет о расходах</a>
-                </nav>
-            </div>
-        </article>
-    </section>
+  <section class="content section">
+    <header>
+      <h1>Отчеты</h1>
+    </header>
+    <article class="media">
+      <div class="media-content">
+        <div class="content">
+          <p>
+            План финансово-хозяйственной деятельности АНО "ЦСП "Шатлык"
+            ("Радость") на 2018 год
+          </p>
+        </div>
+        <nav class="level">
+          <a href="/documents/fhd.jpg" class="button is-link">
+            План финансово-хозяйственной деятельности</a
+          >
+          <a
+            target="_blank"
+            href="https://docs.google.com/gview?url=https://shatlik.ru/documents/actrep.pdf"
+            class="button is-link"
+            >Отчет о деятельности автономных учреждений</a
+          >
+          <a
+            target="_blank"
+            href="https://docs.google.com/gview?url=https://shatlik.ru/documents/Отчет+о+деят-ти+НО.xlsm"
+            class="button is-link"
+            >Отчет о деятельности НО</a
+          >
+          <a
+            target="_blank"
+            href="https://docs.google.com/gview?url=https://shatlik.ru/documents/Отчет+о+расход.дс.xlsm"
+            class="button is-link"
+            >Отчет о расходах</a
+          >
+        </nav>
+      </div>
+    </article>
+  </section>
 </template>
 
 <style lang="scss" scoped>
 .level {
-    justify-content: flex-start;
-    a {
-        margin-right: 20px;
-    }
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  a {
+    margin-right: 20px;
+    margin-bottom: 20px;
+  }
 }
 </style>
